45af3ca69d8d66cbe07bb81eda5f4af9fab89fa0
Lazy Goose <shamal.ma.rss@gmail.com>
2023-12-09 15:16:37 +0300

    refactor: prettify vite.config.ts
    - sort imports
    - remove unnecessary comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
