30a8f6f7cdad7deab8f2d0628a9afc835b184071
Lazy Goose <shamal.ma.rss@gmail.com>
2023-12-10 06:44:52 +0300

    feat: set up msw (mock service worker)

--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from 'vitest'
 
-test('Expect successful test run', () => {
-  expect(true).toBeTruthy()
+test('Expect successful test run', async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+  expect(data).toEqual(['Empty'])
 })
