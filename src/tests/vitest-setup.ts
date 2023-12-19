337d5b596bdfe74896abccc5e40f10323d59d60b
Lazy Goose <shamal.ma.rss@gmail.com>
2023-12-19 09:30:30 +0300

    feat: setup vitest environment in order to mock zustand
    https://github.com/pmndrs/zustand/blob/main/docs/guides/testing.md#vitest

--- src/tests/vitest-setup.ts
+++ src/tests/vitest-setup.ts
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom'
-import { afterAll, afterEach, beforeAll } from 'vitest'
+import { afterAll, afterEach, beforeAll, vi } from 'vitest'
 import { server } from './mocks/server'
+import * as mockZustand from './mocks/zustand'
+
+vi.mock('zustand', () => mockZustand)
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterAll(() => server.close())
