55e25254568f75133644b347dad27dda28d6d9b7
Lazy Goose <shamal.ma.rss@gmail.com>
2023-12-26 15:28:29 +0300

    feat(Footer): impl component layout

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,24 @@ export const pathes = {
   welcomePage: '/welcome',
   signInUpPage: '/sign-in-up',
 }
+
+export const userAvatars = [
+  {
+    href: 'https://github.com/Alyona8891',
+    imgSrc: 'https://avatars.githubusercontent.com/u/79936182?v=4',
+    imgAlt: "Alyona's avatar",
+    active: false,
+  },
+  {
+    href: 'https://github.com/Lev1ossa',
+    imgSrc: 'https://avatars.githubusercontent.com/u/104268412?v=4',
+    imgAlt: "Alexandr's avatar",
+    active: false,
+  },
+  {
+    href: 'https://github.com/lazy-goose',
+    imgSrc: 'https://avatars.githubusercontent.com/u/119844669?v=4',
+    imgAlt: "Maksim's avatar",
+    active: true,
+  },
+] as const
