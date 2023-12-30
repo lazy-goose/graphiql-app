4ba3044fd5c79022e164597d809f114af5a200eb
Lazy Goose <shamal.ma.rss@gmail.com>
2023-12-30 16:13:19 +0300

    feat: impl TabGroup component

---
+++ src/pages/MainPage/TabGroup/defaults.ts
@@ -0,0 +1,10 @@
+export default {
+  Tab: {
+    height: 40,
+    borderSize: 2,
+  },
+  Panel: {
+    paddingBlock: 12,
+    paddingInline: 24,
+  },
+}
---
+++ src/pages/MainPage/TabGroup/index.ts
@@ -0,0 +1,2 @@
+export { default as TabGroup } from './TabGroup'
+export { default as TabGroupDefaults } from './defaults'
