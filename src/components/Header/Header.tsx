cdd009d79f529135a735296aed84425f96f0ff4b
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-02 11:36:58 +0300

    feat: change header style on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import { pathes } from '@/constants/constants'
 import { auth } from '@/firebase'
 import { useBoundStore } from '@/store'
-import { AppBar, Box, Button, Stack, Toolbar } from '@mui/material'
+import {
+  AppBar,
+  Box,
+  Button,
+  Stack,
+  Toolbar,
+  useScrollTrigger,
+} from '@mui/material'
 import { signOut } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Logo from './Logo'
@@ -27,37 +34,47 @@ export function Header(props: { leftSlot?: React.ReactNode }) {
     navigate(pathes.signInUpPage)
   }
 
+  const trigger = useScrollTrigger({
+    threshold: 0,
+    disableHysteresis: true,
+  })
+
   const rightSlot = (
     <Stack direction="row" spacing={1}>
-      {!user && (
-        <Button variant="contained" onClick={handleSignInButton}>
-          SignIn
-        </Button>
-      )}
-      {!user && (
-        <Button variant="contained" onClick={handleSignUpButton}>
-          SignUp
-        </Button>
-      )}
-      {user && (
+      {user ? (
         <Button variant="contained" onClick={handleSignOutButton}>
           SignOut
         </Button>
+      ) : (
+        <>
+          <Button variant="contained" onClick={handleSignInButton}>
+            SignIn
+          </Button>
+          <Button variant="contained" onClick={handleSignUpButton}>
+            SignUp
+          </Button>
+        </>
       )}
     </Stack>
   )
 
+  const opacify = (hex: string) => hex + 25
+  const gradient = () =>
+    `linear-gradient(to right, ${opacify('#42a5f5')}, ${opacify('#ec5db7')})`
+
   return (
     <AppBar
       position="sticky"
-      sx={(theme) => ({
-        backgroundColor: theme.palette.background.paper,
+      sx={{
+        backgroundColor: opacify('#ffffff'),
+        backgroundImage: trigger ? gradient() : undefined,
+        backdropFilter: 'blur(8px)',
         '.MuiToolbar-root': {
           paddingBlock: 1,
           paddingInline: 2,
           boxSizing: 'border-box',
         },
-      })}
+      }}
     >
       <Toolbar>
         <Logo />
