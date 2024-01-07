2b64e89f35d72032a76eaf1245deb36770538ce1
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-07 15:36:15 +0300

    feat: handle queryPrettify on Prettify button click

--- src/components/QueryRequest/QueryRequest.tsx
+++ src/components/QueryRequest/QueryRequest.tsx
@@ -1,20 +1,33 @@
 import { useBaseTheme } from '@/hooks/@CodeMirror/useBaseTheme'
 import { useGraphQlStyle } from '@/hooks/@CodeMirror/useGraphQlStyle'
 import { useBoundStore } from '@/store'
+import { queryPrettify } from '@/utils/queryPrettify'
+import { diagnosticCount } from '@codemirror/lint'
 import { FormatIndentIncrease, PlayArrowRounded } from '@mui/icons-material'
 import { Box, Button, Stack } from '@mui/material'
 import CodeMirror from '@uiw/react-codemirror'
 import { graphql as cm6graphql } from 'cm6-graphql'
+import { useRef } from 'react'
 
 export default function QueryRequest() {
   const schema = undefined
   const queryInput = useBoundStore((state) => state.stringifiedQuery)
   const setQueryInput = useBoundStore((state) => state.setStringifiedQuery)
 
+  const lintErrorsRef = useRef(0)
+
   const baseTheme = useBaseTheme()
   const graphQlStyleExtension = useGraphQlStyle()
 
-  const handlePrettifyButtonClick = () => {}
+  const handlePrettifyButtonClick = () => {
+    if (!lintErrorsRef.current) {
+      try {
+        setQueryInput(queryPrettify(queryInput))
+      } catch {
+        console.error('Unable to prettify query')
+      }
+    }
+  }
   const handleRunButtonClick = () => {}
 
   return (
@@ -31,6 +44,9 @@ export default function QueryRequest() {
         value={queryInput}
         onChange={setQueryInput}
         extensions={[cm6graphql(schema), baseTheme, graphQlStyleExtension]}
+        onUpdate={(cm) => {
+          lintErrorsRef.current = diagnosticCount(cm.state)
+        }}
         basicSetup={{
           highlightActiveLine: false,
           highlightActiveLineGutter: false,
