452f1b763984adda76493ccd19d1500a061d03fd
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-12 23:13:25 +0300

    fix: hide WelcomePage explorer link when user logged out

--- src/pages/WelcomePage/WelcomePageHeader.tsx
+++ src/pages/WelcomePage/WelcomePageHeader.tsx
@@ -2,19 +2,20 @@ import { Header } from '@/components/Header'
 import { OutlineButton } from '@/components/OutlineButton'
 import { RouterPath } from '@/constants'
 import { useLocale } from '@/hooks/useLocale'
+import { useBoundStore } from '@/store'
 import { Link } from 'react-router-dom'
 
 export default function WelcomePageHeader() {
   const {
     locale: { header },
   } = useLocale()
-  return (
-    <Header
-      leftSlot={
-        <OutlineButton component={Link} to={RouterPath.Main} color="secondary">
-          {header.button.explorer}
-        </OutlineButton>
-      }
-    />
+  const isAuthenticated = useBoundStore((state) => state.isAuthenticated())
+
+  const ExplorerButton = (
+    <OutlineButton component={Link} to={RouterPath.Main} color="secondary">
+      {header.button.explorer}
+    </OutlineButton>
   )
+
+  return <Header leftSlot={isAuthenticated ? ExplorerButton : undefined} />
 }
